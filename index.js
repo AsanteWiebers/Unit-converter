e230ce40def3d89964140ec6ae44e346866cb8c7
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-03-24 11:49:38 +0100

    html/css card styling

---
+++ index.js
@@ -0,0 +1,8 @@
+// Save number to a variable in code
+
+// When app Loads, do a calcualtion and display the result
+
+// Round numbers to 3 decimal places
+
+// Extra: add an input for the user to change the number
+//  and automaticall recalculates values when it changers
